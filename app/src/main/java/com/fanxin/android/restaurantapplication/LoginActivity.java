6cb977d9a0519175887469b6c7c02b13108980d4
Xin Fan <xin.x.fan@sony.com>
2019-05-27 15:04:52 +0900

    实现登陆界面

--- app/src/main/java/com/fanxin/android/restaurantapplication/LoginActivity.java
+++ app/src/main/java/com/fanxin/android/restaurantapplication/LoginActivity.java
@@ -1,13 +1,69 @@
 package com.fanxin.android.restaurantapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    //对控件进行声明
+    private EditText mEtUsername;
+    private EditText mEtPassword;
+    private Button mBtnLogin;
+    private TextView mTvRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initView();
+
+        initEvent();
+
+    }
+
+    private void initEvent() {
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //检查登陆成功
+                toOrderActivity();
+
+            }
+        });
+
+        mTvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toRegisterActivity();
+
+            }
+        });
+
+    }
+
+    private void toRegisterActivity() {
+        Intent intent = new Intent(this,RegisterActivity.class);
+        startActivity(intent);
+
+    }
+
+    private void toOrderActivity() {
+        Intent intent = new Intent(this, OrderActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void initView() {
+
+        mEtUsername = findViewById(R.id.id_et_username);
+        mEtPassword = findViewById(R.id.id_et_password);
+        mBtnLogin = findViewById(R.id.id_btn_login);
+        mTvRegister = findViewById(R.id.id_tv_register);
+
     }
 }
