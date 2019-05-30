c06beda56e636b26084c345c10f4c81d7f774ab6
Xin Fan <xin.x.fan@sony.com>
2019-05-30 17:24:05 +0900

    商品列表实现

--- app/src/main/java/com/fanxin/android/restaurantapplication/ui/activity/ProductListActivity.java
+++ app/src/main/java/com/fanxin/android/restaurantapplication/ui/activity/ProductListActivity.java
@@ -3,15 +3,24 @@ package com.fanxin.android.restaurantapplication.ui.activity;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.fanxin.android.restaurantapplication.R;
+import com.fanxin.android.restaurantapplication.bean.Product;
 import com.fanxin.android.restaurantapplication.biz.ProductBiz;
+import com.fanxin.android.restaurantapplication.net.CommonCallback;
+import com.fanxin.android.restaurantapplication.ui.adapter.ProductListAdapter;
 import com.fanxin.android.restaurantapplication.ui.view.refresh.SwipeRefresh;
 import com.fanxin.android.restaurantapplication.ui.view.refresh.SwipeRefreshLayout;
+import com.fanxin.android.restaurantapplication.utils.T;
+import com.fanxin.android.restaurantapplication.vo.ProductItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductListActivity extends BaseActivity {
 
@@ -20,7 +29,16 @@ public class ProductListActivity extends BaseActivity {
     private TextView mTvCount;
     private Button mBtnPay;
 
-    private ProductBiz mProductBiz;
+    private ProductListAdapter mAdapter;
+    private List<ProductItem> mDatas = new ArrayList<>();
+
+    private int mCurrentPage;
+
+    private float mTotalPrice;
+    private int mTotalCount;
+
+
+    private ProductBiz mProductBiz = new ProductBiz();
 
 
 
@@ -66,13 +84,96 @@ public class ProductListActivity extends BaseActivity {
         });
 
 
+        mAdapter.setOnProductListener(new ProductListAdapter.OnProductListener() {
+            @Override
+            public void onProductAdd(ProductItem productItem) {
+                mTotalCount++;
+                mTvCount.setText("数量:"+mTotalCount);
+                mTotalPrice += productItem.getPrice();
+                mBtnPay.setText(String.format("%5.2f",mTotalPrice)+"元　立即支付");
+            }
+
+            @Override
+            public void onProductSub(ProductItem productItem) {
+                mTotalCount--;
+                mTvCount.setText("数量:"+mTotalCount);
+                mTotalPrice -= productItem.getPrice();
+
+                //表示金额之前，要处理金额为显示小数点后两位
+
+                mBtnPay.setText(String.format("%5.2f",mTotalPrice)+"元　立即支付");
+
+            }
+        });
+
+
     }
 
     private void loadMore() {
+        startLoadingProgress();
+        mProductBiz.listByPage(++mCurrentPage, new CommonCallback<List<Product>>() {
+            @Override
+            public void onError(Exception e) {
+                stopLoadingProgress();
+                T.showToast(e.getMessage());
+                mCurrentPage--;
+                mSwipeRefreshLayout.setPullUpRefreshing(false);
+
+            }
+
+            @Override
+            public void onSuccess(List<Product> response) {
+                stopLoadingProgress();
+                mSwipeRefreshLayout.setPullUpRefreshing(false);
+
+                if (response.size() == 0){
+                    T.showToast("没有咯");
+                    return;
+                }
+
+                T.showToast("又找到"+response.size()+"道菜");
+
+                for (Product p: response){
+                    mDatas.add(new ProductItem(p));
+                }
+
+                mAdapter.notifyDataSetChanged();
+
+            }
+        });
 
     }
 
     private void loadDatas() {
+        startLoadingProgress();
+        mProductBiz.listByPage(0, new CommonCallback<List<Product>>() {
+            @Override
+            public void onError(Exception e) {
+                stopLoadingProgress();
+                T.showToast(e.getMessage());
+                mSwipeRefreshLayout.setRefreshing(false);
+
+            }
+
+            @Override
+            public void onSuccess(List<Product> response) {
+                stopLoadingProgress();
+                mSwipeRefreshLayout.setRefreshing(false);
+                mCurrentPage = 0;
+
+                mDatas.clear();
+
+                for (Product p: response){
+                    mDatas.add(new ProductItem(p));
+                }
+
+                mAdapter.notifyDataSetChanged();
+
+                //清空选择的数据，数量价格
+                mTotalPrice = 0;
+                mTotalCount = 0;
+            }
+        });
 
     }
 
@@ -86,6 +187,11 @@ public class ProductListActivity extends BaseActivity {
         //设置控件，上拉，下拉都支持
         mSwipeRefreshLayout.setMode(SwipeRefresh.Mode.BOTH);
         mSwipeRefreshLayout.setColorSchemeColors(Color.RED,Color.BLACK,Color.GREEN,Color.YELLOW);
+
+        mAdapter = new ProductListAdapter(this, mDatas);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mAdapter);
+
     }
 
     @Override
--- app/src/main/java/com/fanxin/android/restaurantapplication/ui/adapter/ProductListAdapter.java
+++ app/src/main/java/com/fanxin/android/restaurantapplication/ui/adapter/ProductListAdapter.java
@@ -6,9 +6,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.fanxin.android.restaurantapplication.R;
 import com.fanxin.android.restaurantapplication.bean.Product;
+import com.fanxin.android.restaurantapplication.config.Config;
+import com.fanxin.android.restaurantapplication.utils.T;
 import com.fanxin.android.restaurantapplication.vo.ProductItem;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -33,11 +39,23 @@ public class ProductListAdapter
     @NonNull
     @Override
     public ProductListItemViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View itemView = mInflater.inflate(R.layout.item_product_list, viewGroup, false);
+        return new ProductListItemViewHolder(itemView);
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull ProductListItemViewHolder productListItemViewHolder, int i) {
+        ProductItem productItem = mProductItems.get(i);
+        Picasso.get()
+                .load(Config.baseUrl+productItem.getIcon())
+                .placeholder(R.drawable.pictures_no)
+                .into(productListItemViewHolder.mIvImage);
+
+        productListItemViewHolder.mTvName.setText(productItem.getName());
+        productListItemViewHolder.mTvCount.setText(productItem.count+"");
+        productListItemViewHolder.mTvLabel.setText(productItem.getLabel());
+        productListItemViewHolder.mTvPrice.setText(productItem.getPrice()+"元/份");
 
     }
 
@@ -47,11 +65,90 @@ public class ProductListAdapter
     }
 
 
-    class ProductListItemViewHolder extends RecyclerView.ViewHolder{
+    //自定义接口
+    public interface OnProductListener{
+        void onProductAdd(ProductItem productItem);
+        void onProductSub(ProductItem productItem);
 
+
+    }
+
+    private OnProductListener mOnProductListener;
+
+    public void setOnProductListener(OnProductListener onProductListener){
+        mOnProductListener = onProductListener;
+    }
+
+    class ProductListItemViewHolder extends RecyclerView.ViewHolder{
+        public ImageView mIvImage;
+        public TextView mTvName;
+        public TextView mTvLabel;
+        public TextView mTvPrice;
+
+        public ImageView mIvAdd;
+        public ImageView mIvSub;
+        public TextView mTvCount;
 
         public ProductListItemViewHolder(@NonNull View itemView) {
             super(itemView);
+            mIvImage = (ImageView) itemView.findViewById(R.id.id_iv_image);
+            mTvName = (TextView) itemView.findViewById(R.id.id_tv_name);
+            mTvLabel = (TextView) itemView.findViewById(R.id.id_tv_label);
+            mTvPrice = (TextView) itemView.findViewById(R.id.id_tv_price);
+            mIvAdd = (ImageView) itemView.findViewById(R.id.id_iv_add);
+            mIvSub = (ImageView) itemView.findViewById(R.id.id_iv_sub);
+            mTvCount = (TextView) itemView.findViewById(R.id.id_tv_count);
+
+
+            //点击以后，跳转到商品详情页
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+            //
+            mIvAdd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    int pos = getLayoutPosition();
+                    ProductItem productItem = mProductItems.get(pos);
+                    productItem.count++;
+                    mTvCount.setText(productItem.count+"");
+                    //回调到Activity
+                    if (mOnProductListener != null){
+                        mOnProductListener.onProductAdd(productItem);
+                    }
+
+                }
+            });
+
+            mIvSub.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = getLayoutPosition();
+                    ProductItem productItem = mProductItems.get(pos);
+
+                    if (productItem.count <= 0){
+                        T.showToast("已经是０了！");
+                        return;
+                    }
+
+                    productItem.count--;
+                    mTvCount.setText(productItem.count+"");
+                    //回调到Activity
+                    if (mOnProductListener != null){
+                        mOnProductListener.onProductSub(productItem);
+                    }
+
+
+
+                }
+            });
+
+
         }
     }
 }
